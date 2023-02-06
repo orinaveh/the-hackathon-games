27c3b60b3b8920ea57812ef4b271c7f014ae3f4b
Ori Naveh <orinaveh1@gmail.com>
2023-02-06 18:10:46 +0200

    wip: select

---
+++ src/lib/components/Select/Select.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import {
+      Menu,
+      MenuButton,
+      MenuItems,
+      MenuItem,
+    } from '@rgossiaux/svelte-headlessui';
+
+    export let title = 'More';
+    export let items: { label: string, value: string }[] = [];
+    export let onClick: () => void;
+  </script>
+  
+  <Menu>
+    <MenuButton>{title}</MenuButton>
+    <MenuItems>
+        {#each items as item}
+            <MenuItem on:click={onClick}>
+            {item.label}
+          </MenuItem>     
+        {/each}
+    </MenuItems>
+  </Menu>
+  
